7e03a6120069781591b73a0bc2e9f55ecd32b14a
kyehuang <kye1234321@gmail.com>
2025-06-04 12:39:08 +0800

    [UPDATE] update the range of angle

--- include/params.hpp
+++ include/params.hpp
@@ -13,8 +13,8 @@
 #define UPDATE_ARM_DELAY 0.01
 const float ARM_MOVEMENT_STEP = 1.0;
 const size_t NUM_OF_SERVOS = 12;
-const uint8_t servoMinAngles[] = {80, 0, 0, 80, 0, 0, 80, 0, 0, 80, 0, 0};
-const uint8_t servoMaxAngles[] = {100, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180};
+const uint8_t servoMinAngles[] = {60, 0, 0, 60, 0, 0, 60, 0, 0, 60, 0, 0};
+const uint8_t servoMaxAngles[] = {120, 180, 180, 120, 180, 180, 120, 180, 180, 120, 180, 180};
 const uint8_t servoInitAngles[] = {90, 180, 0, 90, 0, 180, 90, 0, 180, 90, 180, 0};
 
 const uint8_t offsetr_upper[] = {180, 180, 171, 180, 174, 180, 180, 175, 180, 180, 180, 170};
